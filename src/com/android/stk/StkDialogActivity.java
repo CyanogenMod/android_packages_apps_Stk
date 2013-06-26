3caa7a219bd1c86cfc093996b9003f8da8a5522d
Chris Lei <chrislei@codeaurora.org>
2013-06-25 19:31:49 -0700

    Stk: These changes are related to DISPLAY Text pro cmd
    The following issues related to DISPLAY_TEXT are fixed in this change:
    1. Send the Terminal Response if ResponseNeeded qualifier is set, after
    the default duration else display message for ever.
    
    2 When a high priority Display Text proactive command with infinite
    time out is sent,user has to clear this message. The Terminal Response for
    this command will be sent from Stk framework itself. Stk app receives
    this message and displays it until user clears it.
    
    While the text is being displayed if a next Proactive command is
    received, it should be acted upon immediately.
    
    Change-Id: I8245d50048310633eed8b4011eadfb86a93765c4
    (cherry picked from commit c7eb621e09d2df3c0c79e276ed4af5a938cefaab)
    (cherry picked from commit c4c37873c03b0ff5441ebb1b78c8609a67d8a82f)
    (cherry picked from commit 54e6a2d3d567682c5cf52fb18a65fbf19061e68e)

--- src/com/android/stk/StkDialogActivity.java
+++ src/com/android/stk/StkDialogActivity.java
@@ -18,6 +18,7 @@
 package com.android.stk;
 
 import com.android.internal.telephony.cat.TextMessage;
+import com.android.internal.telephony.cat.CatLog;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -65,18 +66,9 @@ public class StkDialogActivity extends Activity implements View.OnClickListener 
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        initFromIntent(getIntent());
-        if (mTextMsg == null) {
-            finish();
-            return;
-        }
-
         requestWindowFeature(Window.FEATURE_LEFT_ICON);
-        Window window = getWindow();
 
         setContentView(R.layout.stk_msg_dialog);
-        TextView mMessageView = (TextView) window
-                .findViewById(R.id.dialog_message);
 
         Button okButton = (Button) findViewById(R.id.button_ok);
         Button cancelButton = (Button) findViewById(R.id.button_cancel);
@@ -84,35 +76,23 @@ public class StkDialogActivity extends Activity implements View.OnClickListener 
         okButton.setOnClickListener(this);
         cancelButton.setOnClickListener(this);
 
-        setTitle(mTextMsg.title);
-        if (!(mTextMsg.iconSelfExplanatory && mTextMsg.icon != null)) {
-            mMessageView.setText(mTextMsg.text);
-        }
-
-        if (mTextMsg.icon == null) {
-            window.setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
-                    com.android.internal.R.drawable.stat_notify_sim_toolkit);
-        } else {
-            window.setFeatureDrawable(Window.FEATURE_LEFT_ICON,
-                    new BitmapDrawable(mTextMsg.icon));
-        }
     }
 
     public void onClick(View v) {
         String input = null;
-
-        switch (v.getId()) {
-        case OK_BUTTON:
-            cancelTimeOut();
-            sendResponse(StkAppService.RES_ID_CONFIRM, true);
-            finish();
-            break;
-        case CANCEL_BUTTON:
-            cancelTimeOut();
-            sendResponse(StkAppService.RES_ID_CONFIRM, false);
-            finish();
-            break;
+        if (mTextMsg.responseNeeded) {
+             switch (v.getId()) {
+                 case OK_BUTTON:
+                     cancelTimeOut();
+                     sendResponse(StkAppService.RES_ID_CONFIRM, true);
+                     break;
+                 case CANCEL_BUTTON:
+                     cancelTimeOut();
+                     sendResponse(StkAppService.RES_ID_CONFIRM, false);
+                     break;
+             }
         }
+        finish();
     }
 
     @Override
@@ -130,6 +110,44 @@ public class StkDialogActivity extends Activity implements View.OnClickListener 
     @Override
     public void onResume() {
         super.onResume();
+
+        initFromIntent(getIntent());
+        if (mTextMsg == null) {
+            finish();
+            return;
+        }
+
+        Window window = getWindow();
+
+        TextView mMessageView = (TextView) window
+                .findViewById(R.id.dialog_message);
+
+        setTitle(mTextMsg.title);
+
+        if (!(mTextMsg.iconSelfExplanatory && mTextMsg.icon != null)) {
+            mMessageView.setText(mTextMsg.text);
+        }
+
+        if (mTextMsg.icon == null) {
+            window.setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
+                    com.android.internal.R.drawable.stat_notify_sim_toolkit);
+        } else {
+            window.setFeatureDrawable(Window.FEATURE_LEFT_ICON,
+                    new BitmapDrawable(mTextMsg.icon));
+        }
+
+
+        /*
+         * If the userClear flag is set and dialogduration is set to 0, the display Text
+         * should be displayed to user forever until some high priority event occurs
+         * (incoming call, MMI code execution etc as mentioned under section
+         * ETSI 102.223, 6.4.1)
+         */
+        if (StkApp.calculateDurationInMilis(mTextMsg.duration) == 0 &&
+            !mTextMsg.responseNeeded && mTextMsg.userClear) {
+            CatLog.d(this, "User should clear text..showing message forever");
+            return;
+        }
         startTimeOut(mTextMsg.userClear);
     }
 
@@ -162,6 +180,12 @@ public class StkDialogActivity extends Activity implements View.OnClickListener 
         mTextMsg = savedInstanceState.getParcelable(TEXT);
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        CatLog.d(this, "onNewIntent - updating the same Dialog box");
+        setIntent(intent);
+    }
+
     private void sendResponse(int resId, boolean confirmed) {
         Bundle args = new Bundle();
         args.putInt(StkAppService.OPCODE, StkAppService.OP_RESPONSE);
@@ -188,6 +212,7 @@ public class StkDialogActivity extends Activity implements View.OnClickListener 
     }
 
     private void startTimeOut(boolean waitForUserToClear) {
+
         // Reset timeout.
         cancelTimeOut();
         int dialogDuration = StkApp.calculateDurationInMilis(mTextMsg.duration);
