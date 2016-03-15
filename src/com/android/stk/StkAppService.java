ae417d63653d3ca43f7d30bc71a2fb64be5effe8
Umashankar Godachi <umasha@codeaurora.org>
2016-03-15 16:07:45 +0530

    Fix phone process crash during factory reset.
    In the present code of StkAppService, while doing
    the factory reset phone process is crashing due to
    ArrayIndexOutOfBoundsEception caused by no running
    tasks.
    
    Fix: In StkAppService, check for topactitvity only if
    there are running tasks present.
    
    Change-Id: I27d256c0a4434f090d6dc7060d09bf58106f7d15
    CRs-Fixed: 988098

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -866,8 +866,11 @@ public class StkAppService extends Service implements Runnable {
     private boolean isTopOfStack() {
         ActivityManager mAcivityManager = (ActivityManager) mContext
                 .getSystemService(ACTIVITY_SERVICE);
-        String currentPackageName = mAcivityManager.getRunningTasks(1).get(0).topActivity
-                .getPackageName();
+        String currentPackageName = null;
+        List<RunningTaskInfo> tasks = mAcivityManager.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            currentPackageName = tasks.get(0).topActivity.getPackageName();
+        }
         if (null != currentPackageName) {
             return currentPackageName.equals(PACKAGE_NAME);
         }
